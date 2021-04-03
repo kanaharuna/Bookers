09aa98a307c5d6db85745bf9c4401b7e9d5690f5
kanaharuna <ec2-user@ip-172-31-27-220.ec2.internal>
2021-04-03 13:29:13 +0000

    [Add]バリデーションまで追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-    flash[:notice] = "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -20,10 +24,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy

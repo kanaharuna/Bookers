f71318b1a9d5c17c56efc17243f326a531914789
kanaharuna <ec2-user@ip-172-31-27-220.ec2.internal>
2021-04-03 06:10:22 +0000

    [Add]編集機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
@@ -13,16 +13,20 @@ class BooksController < ApplicationController
     book.save
     redirect_to book_path(book.id)
   end
-  
+
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
   end
-  
+
   private
 
   def book_params

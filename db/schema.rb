7e65536c665c99fb8dca83c7c5fa8de3f99ad98a
kanaharuna <ec2-user@ip-172-31-27-220.ec2.internal>
2021-04-04 01:09:06 +0000

    [Finish]3-8

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_04_02_041233) do
 
   create_table "books", force: :cascade do |t|
-    t.string "title",null: false
-    t.string "body",null: false
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
